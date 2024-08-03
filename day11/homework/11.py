669a31aa1b1c694f6ffe361e21acff927d291240
marta220313 <mamasgogobasku@gmail.com>
2024-08-03 13:37:15 +0400

    commit

---
+++ day11/homework/11.py
@@ -0,0 +1,39 @@
+# კომენტარების გამოყენებით ახსენით რა არის string, integer, float
+
+#name = "marta" #ეს არის str (string) ტიპის ცვლადი
+#age = 11 #ეს არის int (integer) იგივე მთელი რიცხვი
+#height = 1.50 #ეს არის float ტიპის ცვლადი (ათწილადი)
+
+
+#კომენტარების გამოყენებით ახსენით რისთვის გვჭირდება ინფორმაციის დაყოფა მონაცემთა ტიპებად
+
+#იმიტომ გვჭირდება ინფორმაციის დაყოფა მონაცემთა ტიპებად, რადგან ყველა კოდი რომ შეურიო ერთმანეთში არაფერიც არ იქნება გამოტანილი ეკრანზე.
+
+#დაწერეთ 20 კონკატინაციის (სთრინგების შეერთების) მაგალითი
+print("hello + world")
+print("marta" + "11")
+print("paint + paper")
+print("cat + mouse")
+print("GOA +  best")
+print("1 + 1")
+print("nose + smell")
+print("hair + brusher")
+print("int + float")
+print("name + surname")
+print("day + night")
+print("int + str")
+print("break + case")
+print("continue + film")
+print("awaik + up")
+print("home + work")
+print("game + over")
+print("play + gittar")
+print("error + code")
+print("Key + Error")
+
+#მომხმარებლისგან მიიღეთ ინფორმაცია input ბრძანების გამოყენებით და შეინახეთ ცვლადში. შემდეგ დაბეჭდეთ ამ ცვლადის მნიშვნელობა.
+
+name = input("what is your name:")
+surname = input("what is your surename:")
+age = input("what is your age:")
+print(str, str, int)
