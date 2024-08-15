c730c09cae72328739579a924511b2247041b3de
marta220313 <mamasgogobasku@gmail.com>
2024-08-15 17:32:11 +0400

    commit

---
+++ day13/classwork/13.py
@@ -0,0 +1,14 @@
+num1="218"
+print(type(num1))
+
+num2="386"
+print(type(num2))
+
+num3="110"
+print(type(num3))
+
+num4="609"
+print(type(num4))
+
+num5="583"
+print(type(num5))
---
+++ day14/classwork/14.py
@@ -0,0 +1,64 @@
+#printის გამოყენებით გამოიტანეთ 3-3 float, int და stringი.
+
+print("marta")
+print("matusa")
+print("tagvi")
+
+print(42)
+print(22)
+print(11)
+
+print(64.64)
+print(25.47)
+print(15.94)
+
+#type()ის გამოყენებით გამოიტანეთ int float და str სმა სამჯერ.
+
+name = "ani"
+print(str)
+
+name = "marta"
+print(str)
+
+name = "salome"
+print(str)
+
+name = 20
+print(int)
+
+name = 32
+print(int)
+
+name = 40
+print(int)
+
+name = 31.3
+print(float)
+
+name = 54.6
+print(float)
+
+name =30.5 
+print(float)
+
+#მომხმარებელს შემოატანინეთ 2 ცვლადი შემდეგ ეს ორი ცვლადი გადააქციეთ ინტეჯერათ და დაუმათეთ ერთმანეთს ბოლო 
+#მათი ჯამი გამოიტანეთ printის დახმარებით.
+
+first = input("sheiyvanet cvladi")
+second = input("sheiyvanet cvladi")
+print(int(first) + int(second))
+
+#inputის გამოყენებით მომხმარებელს შემოატანინეთ მისი სახელი, გვარი, ასაკი და ჰობი, ეს ყველაფერი შემოატანინეთ 
+#კიდევ ერთხელ ოღონდ ამჯერად მის მეგობარზე შემდეგ printის გამოყენებით ეს ყველაფერი გააერთიანე და შექმენი
+#ერთი დიდი წინადადება რასაც გააკეთებთ ყველაფერი ახსენით კომენტარებით.
+name = input("what is your name:")
+surname = input("what is your surname:")
+age = input("what is your age:")
+hobbie = input("what is your hobbie:")
+print(name, surname, age, hobbie)
+
+name2= input("what is your friend  name:")
+surname2 = input("what is your friend surname:")
+age2 = input("what is your friend age:")
+hobbie2 = input("what is your friend hobbie:")
+print(name, surname, age, hobbie)
