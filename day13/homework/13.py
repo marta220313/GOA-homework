e3d302225691c0c922883879023a16c70255a399
marta220313 <mamasgogobasku@gmail.com>
2024-08-17 20:36:17 +0400

    commit

--- day13/homework/13.py
+++ day13/homework/13.py
@@ -1,3 +1,304 @@
-მომხმარებელს შემოატანინეთ ასაკი და შეინახეთ ცვლადში. 
-შემდეგ ეს ინფორმაცია გადააქციეთ მთელ რიცხად და დაუბეჭდეთ 5 წელში
-რამდენი წლის იქნება.
+#მომხმარებელს შემოატანინეთ ასაკი და შეინახეთ ცვლადში. შემდეგ ეს ინფორმაცია გადააქციეთ მთელ რიცხად და დაუბეჭდეთ 5 წელში რამდენი წლის იქნება.
+
+age = int(input("შემოიტანეთ მომხმარებლის ასაკი:"))
+print(age + 5)
+
+
+#ჩამოწერეთ 20 ცვლადი და თითოეულში შეინახეთ string, integer ან float. შემდეგ თითოელზე გამოიყენეთ type ინსტრუქცია და დაბეჭდეთ ცვლადში რომელი მონაცემთა ტიპი ინახება.
+
+(string)
+
+num1 = "marta"
+print(str(num1))
+
+num2 = "6"
+print(str(num2))
+
+num3 = "54"
+print(str(num3))
+
+num4 = "47"
+print(str(num4))
+
+num5 = "ani"
+print(str(num5))
+
+num6 = "mate"
+print(str(num6))
+
+num7 = "salome"
+print(str(num7))
+
+num8 = "nika"
+print(str(num8))
+
+num9 = "13"
+print(str(num9))
+
+num10 = "rare"
+print(str(num10))
+
+num11 = "dragon"
+print(str(num11))
+
+num12 = "gega"
+print(str(num12))
+
+num13 = "natali"
+print(str(num13))
+
+num14 = "mariami"
+print(str(num14))
+
+num15 = "tornike"
+print(str(num15))
+
+num16 = "book"
+print(str(num16))
+
+num17 = "school"
+print(str(num17))
+
+num18 = "bag"
+print(str(num18))
+
+num19 = "pencil"
+print(str(num19))
+
+num20 = "pen"
+print(str(num20))
+
+(integer)
+
+num1 = 1
+print(int(num1))
+
+num2 = 2
+print(int(num2))
+
+num3 = 3
+print(int(num3))
+
+num4 = 4
+print(int(num4))
+
+num5 = 5
+print(int(num5))
+
+num6 = 6
+print(int(num6))
+
+num7 = 7
+print(int(num7))
+
+num8 = 8
+print(int(num8))
+
+num9 = 9
+print(int(num9))
+
+num10 = 10
+print(int(num10))
+
+num11 = 11
+print(int(num11))
+
+num12 = 12
+print(int(num12))
+
+num13 =13 
+print(int(num13))
+
+num14 = 14
+print(int(num14))
+
+num15 = 15
+print(int(num15))
+
+num16 = 16
+print(int(num16))
+
+num17 = 17
+print(int(num17))
+
+num18 = 18
+print(int(num18))
+
+num19 = 19
+print(int(num19))
+
+num20 = 20
+print(int(num20))
+
+(float)
+
+num1 = 1.1
+print(float(num1))
+
+num2 = 2.2
+print(float(num2))
+
+num3 = 3.3
+print(float(num3))
+
+num4 = 4.4
+print(float(num4))
+
+num5 = 5.5
+print(float(num5))
+
+num6 = 6.6
+print(float(num6))
+
+num7 = 7.7
+print(float(num7))
+
+num8 = 8.8
+print(float(num8))
+
+num9 = 9.9
+print(float(num9))
+
+num10 = 10.10
+print(float(num10))
+
+num11 = 11.11
+print(float(num))
+
+num12 = 12.12
+print(float(num12))
+
+num13 = 13.13
+print(float(num13))
+
+num14 = 14.14
+print(float(num14))
+
+num15 = 15.15
+print(float(num15))
+
+num16 = 16.16
+print(float(num16))
+
+num17 = 17.17
+print(float(num17))
+
+num18 = 18.18
+print(float(num18))
+
+num19 = 19.19
+print(float(num19))
+
+num20 = 20.20
+print(float(num20))
+
+
+#მონაცემთა ტიპის გადაყვანის შესახებ ჩამოწერეთ 30 მაგალითი: 10 int-ზე, 10 float-ზე და 10 string-ზე.
+
+(string)
+
+num1 = "1"
+print(str(num1))
+
+num2 = "2"
+print(str(num2))
+
+num3 = "3"
+print(str(num3))
+
+num4 = "4"
+print(str(num4))
+
+num5 = "5"
+print(str(num5))
+
+num6 = "6"
+print(str(num6))
+
+num7 = "7"
+print(str(num7))
+
+num8 = "8"
+print(str(num8))
+
+num9 = "9"
+print(str(num9))
+
+num10 = "10"
+print(str(num10))
+
+(integer)
+
+num1 = 11
+print(int(num1))
+
+num2 = 22
+print(int(num2))
+
+num3 = 33
+print(int(num3))
+
+num4 = 44
+print(int(num4))
+
+num5 = 55
+print(int(num5))
+
+num6 = 66
+print(int(num6))
+
+num7 = 77
+print(int(num7))
+
+num8 = 88
+print(int(num8))
+
+num9 = 99
+print(int(num9))
+
+num10 = 1010
+print(int(num10))
+
+(float)
+
+num1 = 11.11
+print(float(num1))
+
+num2 = 22.22
+print(float(num2))
+
+num3 = 33.33
+print(float(num3))
+
+num4 = 44.44
+print(float(num4))
+
+num5 = 55.55
+print(float(num))
+
+num6 = 66.66
+print(float(num6))
+
+num7 = 77.77
+print(float(num7))
+
+num8 = 88.88
+print(float(num8))
+
+num9 = 99.99
+print(float(num9))
+
+num10 = 1010.1010
+print(float(num10))
+
+
+
+
+
+
+
+
+
+
+
+
