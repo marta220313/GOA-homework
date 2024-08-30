85e969a2a71c4c695888f6608362f11415af43e8
marta220313 <mamasgogobasku@gmail.com>
2024-08-30 16:46:01 +0400

    second commit

---
+++ day16/classwork/16,2.py
@@ -0,0 +1,27 @@
+#საკლასო დავალება2: ერთ ცვლადში შეინახეთ მომხმარებლის შემოტანილი რიცხვი integer მონაცემთა ტიპში. მეორე ცვლადშიც შეინახეთ მეორეჯერ შემოტანილი 
+#რიცხვი,ესეც integer-ის სახით. 
+#ამ ორ ცვლადზე მოახდინეთ შედარებები ნასწავლი ოპერატორებით - >, >=, <, <=, ==, !=
+
+a = int(input("enter your number: "))
+
+b = int(input("enter your number: "))
+
+
+print(b > a) #true
+
+
+print(a < b) #true
+
+
+print(b >= a) #true
+
+
+print(b <= b) #true
+
+
+print(a == a) #true
+
+
+print(b != b) #false
+
+
