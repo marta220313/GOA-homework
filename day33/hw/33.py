82de8bde1f81de9500a8f7d0a1f986f466fd1518
marta220313 <mamasgogobasku@gmail.com>
2024-12-21 21:14:01 +0400

    commit

---
+++ day33/hw/33.py
@@ -0,0 +1,16 @@
+list1 = ["roof", 4.5, 7, 6543, "youtobe chanell "GOA"", 89, "SALE", "GOA", 6, "marta", 11, 12.89, "13.345678", "useful", 15.7, 46, "seventeen", 18.35, 3907, 20.13456789]
+print(list1)
+
+
+list2 = [23456, 78910, 11233, 45, 98765432169]
+list2[0] = 3
+list2[1] = 1.123456789
+list2[2] = 6548
+list2[3] = "hello world!"
+list2[4] = "goa best"
+print(list2)
+
+
+
+
+
