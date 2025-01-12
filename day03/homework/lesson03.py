a5db05b7932033a12ac336b317831848f2574f7d
marta220313 <mamasgogobasku@gmail.com>
2025-01-12 18:58:44 +0400

    commit

--- day03/homework/lesson03.py
+++ day03/homework/lesson03.py
@@ -1,14 +1,3 @@
-num1=22
-num2=18
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
-
-
-num1=15
-num2=5
-print(num1 + num2)
-print(num1 - num2)
-print(num1 * num2)
-print(num1 / num2)
+print("name: მართა")
+print("lastname: ყიფშიძე")
+print("age: 11")
---
+++ day04/homework/lesson04.py
@@ -0,0 +1,14 @@
+num1=22
+num2=18
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+num1=15
+num2=5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
