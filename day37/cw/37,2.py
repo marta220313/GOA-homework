f467a340a00d016c786b08bd66d73516c96c48e8
marta220313 <mamasgogobasku@gmail.com>
2025-01-24 21:19:23 +0400

    commit

---
+++ day37/cw/37,2.py
@@ -0,0 +1,9 @@
+def sum_or_multiply(num1, num2):
+    if num1 >50:
+        print(num1 * num2)
+    else:
+        print(num1 + num2)
+
+sum_or_multiply(34, 89)
+sum_or_multiply(5, 6)
+sum_or_multiply(456, 345)
---
+++ day37/cw/37.py
@@ -0,0 +1,6 @@
+
+
+def multiply_numbers(num1, num2):
+    print(num1 * num2)
+
+multiply_numbers(5, 6)
