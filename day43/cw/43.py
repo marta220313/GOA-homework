8bbc72b73812b00e781a3ae6b26071e4925752e6
marta220313 <mamasgogobasku@gmail.com>
2025-03-28 23:48:19 +0400

    commit

---
+++ day43/cw/43.py
@@ -0,0 +1,51 @@
+#შექმენით ფუნქცია სახელად print_arr() 
+#რომელიც არგუმენტად იღებს ლისთს და გამოპრინტავს 
+#მის ყველა ელემენტს
+
+name = hi, water+melon
+
+def print_arr(name):
+    print({name}, i like you <3)
+    
+#2) შექმენით ფუნქცია სახელად check() რომელიც 
+#აბრუნებს True თუ არგუმენტად გადაცემული სტრინგის 
+#სიგრძე მეტია 4-ზე და ნაკლები -8-ზე თ არადა აბრუნებს 
+#False
+
+def check():
+        num = int(input("enter your number: "))
+    if num == +4 and -8:
+        print("TRUE")
+    else:
+        print("FALSE")
+
+#3) შექმენით ფუნქცია სახელად no_spaces() რომელსაც 
+# გადაეცემა სტრინგი და აბრუნებს ამ სტრინგს სადაც " " 
+# ( სფეისები ) მაგივრად იქნება "-" 
+
+def no_spaces():
+    result = ""
+    for letter in sentence:
+          if letter == " ":
+                res += "-"
+          else:
+                res += letter
+        return res
+
+print(no_spaces(hi, my name is marta))
+
+
+#4) შექმენით ფუნქცია სახელად Is_best_academy() 
+# რომელიც პრინტავს 'Goal-Oriented-Academy"-ს
+
+name = "Goal-Oriented-Academy"
+
+def Is_best_academy(name):
+    print(hi, {name})
+
+#5)შექმენით ფუნქცია სახლეად complex_calc() რომელიც 
+# დააბრუნებს ორი არგუმენტის ჯამს თუ პირველი 
+# არგუმენტი ლუწია ხოლო დააბრუნებს ნამრავლს 
+# თუ პირველი არგუმენტი კენტია
+
+
---
+++ day43/hw/43.py
@@ -0,0 +1,5 @@
+print(int(input"enter biggest number: "))
+if number == %2==0:
+    print(True)
+else:
+    print(False):
